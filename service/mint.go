9bfa1abbd18c239332be928b8c927cd2407e76ef
bonedaddy <boned4ddy@protonmail.com>
2020-09-22 14:36:24 -0700

    more updates

--- service/mint.go
+++ service/mint.go
@@ -6,12 +6,30 @@ import (
 )
 
 func (s *Service) handleNewMint(paymentID, ethAddress string) {
+	var confirmed bool
 	for {
 		resp, err := s.mc.GetPayments(s.walletName, paymentID)
 		if err != nil {
 			goto SLEEP
 		}
-		log.Printf("payments: %#v\n", resp.Payments)
+		if len(resp.Payments) < 0 {
+			goto SLEEP
+		}
+		if len(resp.Payments) > 1 {
+			log.Println("ERROR: more than 2 payments found to this address")
+		}
+		confirmed, err = s.mc.TxConfirmed(s.walletName, resp.Payments[0].TxHash)
+		if err != nil {
+			log.Println("failed to check tx confirmation status: ", err)
+		}
+		if !confirmed {
+			log.Println("tx is not yet confirmed")
+		}
+		if confirmed {
+			// TODO: handle confirmation
+			log.Println("tx is confirmed")
+			return
+		}
 		goto SLEEP
 	SLEEP:
 		time.Sleep(time.Minute * 2)
--- service/service_test.go
+++ service/service_test.go
@@ -1,11 +1,12 @@
 package service
 
 import (
+	"bytes"
 	"context"
 	"encoding/json"
 	"io/ioutil"
-	"log"
 	"net/http"
+	"os"
 	"strings"
 	"testing"
 	"time"
@@ -14,6 +15,7 @@ import (
 	"github.com/bonedaddy/wxmr/rpc"
 	"github.com/ethereum/go-ethereum/accounts/abi/bind"
 	"github.com/ethereum/go-ethereum/ethclient"
+	"github.com/monero-ecosystem/go-monero-rpc-client/wallet"
 	"github.com/stretchr/testify/require"
 )
 
@@ -24,6 +26,7 @@ var (
 	gethRPC          = "http://127.0.0.1:8545"
 	dbPath           = "test.db"
 	ethKeyContents   = `{"address":"f2ea9ce3a27862650a8d40d98329dc0bd403a0c3","crypto":{"cipher":"aes-128-ctr","ciphertext":"75b1c0181fee4c7fa634a49bac74dacb12dcc27ec157e8549b6374924c5b1272","cipherparams":{"iv":"87117ca98fd0652db342a549889be7fe"},"kdf":"scrypt","kdfparams":{"dklen":32,"n":262144,"p":1,"r":8,"salt":"032f744f960f549b4fc6d64177622fa1d34b16e8a4a106812d5ddc37487cb435"},"mac":"f468643296c390300e4ff808b9db4831275565c813c77cc64283adaaa1bcdffd"},"id":"c74bc71b-28a0-42cd-9474-4ffc98276d84","version":3}`
+	ethAddress       = "0xf2ea9ce3a27862650a8d40d98329dc0bd403a0c3"
 )
 
 func TestService(t *testing.T) {
@@ -41,6 +44,7 @@ func TestService(t *testing.T) {
 		database.Close()
 		ec.Close()
 		mc.Close()
+		os.Remove(dbPath)
 	})
 	auth, err := bind.NewTransactor(strings.NewReader(ethKeyContents), "")
 	srv, err := NewService(serviceAddr, moneroWalletName, mc, ec, auth, database)
@@ -50,16 +54,34 @@ func TestService(t *testing.T) {
 	time.Sleep(time.Second)
 
 	callURL := "http://" + serviceAddr
+
 	hc := &http.Client{}
 	req, err := http.NewRequest("GET", callURL+"/deposit_address", nil)
 	require.NoError(t, err)
+
 	resp, err := hc.Do(req)
 	require.NoError(t, err)
 	defer resp.Body.Close()
+
 	data, err := ioutil.ReadAll(resp.Body)
 	require.NoError(t, err)
-	log.Println(string(data))
 	var depResp ResponseGetDepositAddress
 	require.NoError(t, json.Unmarshal(data, &depResp))
+	t.Logf("deposit request: %s", depResp)
 
+	if resp, err := mc.Transfer(rpc.TransferOpts{Priority: wallet.PriorityElevated, WalletName: moneroWalletName, Destinations: map[string]uint64{depResp.Address: wallet.Float64ToXMR(0.1)}}); err != nil {
+		t.Fatal(err)
+	} else {
+		t.Log("transfer hash: ", resp.TxHash)
+	}
+
+	data, err = json.Marshal(&RequestMint{PaymentID: depResp.PaymentID, EthAddress: ethAddress})
+	require.NoError(t, err)
+
+	req, err = http.NewRequest("POST", callURL+"/mint", bytes.NewReader(data))
+	require.NoError(t, err)
+	_, err = hc.Do(req)
+	require.NoError(t, err)
+
+	time.Sleep(time.Minute * 20)
 }
